8b7ae00e18e5eae7be903df096929bc2251fc574
indrabhanverma <indrabhanv39@gmail.com>
2023-03-27 13:50:43 +0530

    ss

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -15,7 +15,7 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::get(); 
-        dd($projects);
+        
         return response()->json($projects);
        
 
